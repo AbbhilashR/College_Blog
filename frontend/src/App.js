f213936d4e9e5d5ab7a651f10d9bdeac91385fe2
Sathish -Official <sathishkumar25800@gmail.com>
2022-09-15 18:32:10 +0530

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,30 @@ import './App.css';
 
 function App() {
   return (
+    <div className="App">
       <header className="App-header">
         <center>
           <span className="Bar">
           <button className="title " onClick={(event)=>{
             window.location.reload();
           }}>Campus Blogs</button>
-
           </span>
           </center>
-      </header>
-  );
+          </header>
+          <div>
+            <form className="App-form" onSubmit={(event)=>{ 
+              event.preventDefault();
+              console.log(event);
+              }}>
+                <input type="text" /><br/>
+                <input type="password" /><br/>
+                <input type="submit" />
+              </form>
+          </div>
+
+
+          </div>
+);
 }
 
 export default App;
