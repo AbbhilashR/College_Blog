423cd7f86d1817b359b2f19ab243b4d168185459
Sathish -Official <sathishkumar25800@gmail.com>
2022-09-15 15:41:32 +0530

    initial commit 1

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+      <header className="App-header">
+          <span className="Bar">
+            Campus Blogs
+          </span>
+      </header>
+  );
+}
+
+export default App;
