2dcb64e1ebdcdd947376c1d7865e395bd16a0b38
Sathish -Official <sathishkumar25800@gmail.com>
2022-09-15 18:32:33 +0530

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function App() {
   return (
@@ -13,17 +15,42 @@ function App() {
           </span>
           </center>
           </header>
-          <div>
+          <div className='login'>
             <form className="App-form" onSubmit={(event)=>{ 
               event.preventDefault();
               console.log(event);
               }}>
-                <input type="text" /><br/>
-                <input type="password" /><br/>
-                <input type="submit" />
-              </form>
+              <TextField
+                required
+                id="outlined-required"
+                label="Email"
+                placeholder="Email"
+                defaultValue="  "
+              />
+              <div></div>
+                {/* <input type="text" /><br/> */}
+                {/* <label>Password:</label><br/> */}
+                <br/>
+                <TextField 
+                required
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          placeholder="Password"
+          // autoComplete="current-password"
+        />
+        
+        <div></div>
+        <br/>
+                {/* <input type="password" /><br/> */}
+                <Button variant="contained" className='submit'>Submit</Button>
+                <div></div> <br/>
+                <span>Don't Have an account yet? <b>
+              <span><a href="/">Sign up!</a></span></b>
+              </span></form>
+            
           </div>
-
+          
 
           </div>
 );
