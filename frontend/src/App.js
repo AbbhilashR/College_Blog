ccde6ee8f0ba0866c04f3859e211596c3b477398
Sathish -Official <sathishkumar25800@gmail.com>
2022-09-15 16:09:02 +0530

    Initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+
+
 function App() {
   return (
       <header className="App-header">
+        <center>
           <span className="Bar">
           <button className="title " onClick={(event)=>{
             window.location.reload();
           }}>Campus Blogs</button>
+
           </span>
+          </center>
       </header>
   );
 }
